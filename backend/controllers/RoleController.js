0f2e25cba275743dbda9958dee124a775dfc0e48
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-09-27 13:13:49 +0300

    role validatio added

--- backend/controllers/RoleController.js
+++ backend/controllers/RoleController.js
@@ -3,10 +3,9 @@ const { addRole } = require("../models/RoleModel");
 const { ZodError } = require("zod");
 const { roleSchema } = require("../zod/Validation");
 
-// create permission
+// create role
 const createRole = async (req, res) => {
   try {
-    // Validate request body
     const validatedData = roleSchema.parse(req.body);
 
     const { name, permission } = validatedData;
@@ -14,12 +13,10 @@ const createRole = async (req, res) => {
     res.status(201).json(newRole);
   } catch (error) {
     if (error instanceof ZodError) {
-      // Handle validation errors
       res
         .status(400)
         .json({ error: "Validation error", details: error.errors });
     } else {
-      // Handle other errors
       console.error(error);
       res.status(500).json({ error: "Server error" });
     }
